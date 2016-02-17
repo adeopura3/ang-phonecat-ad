41773b68db139b8ad68f5a45ec2e203d3666bdc0
adeopura3 <ashish.deopura@gmail.com>
2016-02-17 13:20:33 -0500

    Filtering Repeaters (Step 3)
    Added query box
    Search based on query using filter in angular
    Change html title to add query as part of title

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,11 +1,53 @@
 'use strict';
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
+// end to end testsa
+describe('PhoneCat Application', function() {
 
-describe('my app', function() {
+  describe('Phone List View', function() {
 
-  beforeEach(function() {
-    browser.get('app/index.html');
+    beforeEach(function() {
+      // get to the page
+      browser.get('app/index.html');
+    });
+    
+  
+  var phoneList = element.all(by.repeater('phone in phones'));
+  var query = element(by.model('query'));
+  
+  it ("should show only the filtered phones as user types into search box", function(){
+      // by default we have 3 phones
+      expect(phoneList.count()).toBe(3);
+      
+      // send query
+      query.clear();
+      query.sendKeys('XOOM');
+      expect(phoneList.count()).toBe(2);
+      
+      // send query
+      // note that filter is case insensitive
+      // and nexus will match Nexus
+      query.clear();
+      query.sendKeys('nexus');
+      expect(phoneList.count()).toBe(1);
+      
+      // send query
+      query.clear();
+      query.sendKeys('random');
+      expect(phoneList.count()).toBe(0);
+      
+    });
+  
+  it ("should show the title based on the text currently in the search filter", function(){
+    query.clear();
+    expect(browser.getTitle()).toMatch(/Google Phone Gallery:\s*$/);
+    
+    query.clear();
+    query.sendKeys('motorola');
+    expect(browser.getTitle()).toMatch(/Google Phone Gallery: motorola$/);
+  });
+    
+  
   });
 
 });
