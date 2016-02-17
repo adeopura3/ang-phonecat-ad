d7f8ceb0327d5ad56eab78ca0e5e7dc40cac582f
adeopura3 <ashish.deopura@gmail.com>
2016-02-17 12:24:20 -0500

    Angular Templates (Step 2)
    Adding angular binding to show phones
    Adding unit test to check if the number of phones match the expected
    number

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,11 +1,16 @@
 'use strict';
 
-/* jasmine specs for controllers go here */
-
-describe('controllers', function() {
-
-  it("should do something", function() {
-
-  });
+// controller tests
+describe('PhoneListCtrl Controller', function() {
+  
+  beforeEach(module('phonecatApp'))
+  
+  it('should have 3 phones', inject(function($controller){
+    var scope = {};
+    var phoneListController = $controller('PhoneListCtrl', {$scope: scope});
+    
+    expect(scope.phones.length).toBe(3);
+    
+  }));
 
 });
