a4e1439ae00f705efdaf4fb935890cb2ce7615de
adeopura3 <ashish.deopura@gmail.com>
2016-02-18 00:07:11 -0500

    Animations (Step 12)
    Animations

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
 'use strict';
 
 /* App Module */
-var phonecatApp = angular.module('phonecatApp', ['ngRoute', 'ngResource']);
+var phonecatApp = angular.module('phonecatApp', ['ngRoute', 'ngResource', 'ngAnimate']);
